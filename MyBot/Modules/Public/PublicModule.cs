57be62fd4958089ac85bbcd50e0f68a610909be6
MisterMorde <morde@ninjafatcats.com>
2017-05-31 10:47:57 +1000

    initial commit

---
+++ MyBot/Modules/Public/PublicModule.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyBot.Modules.Public
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("test")] //Command Name
+        [Remarks("Tests your bot to see if it worked ;)")] //Summary for your command. it will not add anything.
+        public async Task Test()
+        {
+            await ReplyAsync("Successfully Conducted test. Also , you should thank our god ; Frost, for giving you this project."); //makes the bot reply back!
+        }
+        [Command("credits")]
+        [Remarks("Credits for this project")]
+        public async Task Credits()
+        {
+            var embed = new EmbedBuilder()
+            {
+                Color = new Color(245, 232, 31)
+            };
+            embed.Title = $"Credits:";
+            embed.Description = $"~Frost \n ~Morde";
+            embed.WithFooter(new EmbedFooterBuilder().WithIconUrl("https://cdn.discordapp.com/attachments/275377268728135680/318819504467476483/FrostBot_LOGO_png.png").WithText($"As Requested By {Context.User.Username}."));
+            await Context.Channel.SendMessageAsync("", embed: embed);
+        }
+    }
+
+
+}
+
+
+/*
+    Hello Everyone! This is a bot template i made B/c the one lunarlite made in CWS forum is shit and outdated!
+
+    This one is updated to the latest version of 1.0: RC3! 
+        
+
+    use the Pre-Condition ===> [Command("Name")] To add a name to a command! Remember that this Pre-Condition will not trigger unless you have your prefix before the said command! 
+    (Prefix in CommandHandler.cs , Line 58)
+
+    Additional Pre-Attributes to Consider:
+
+    [Remarks("Summary")] ===> Adds a summary to your designated command. will not trigger anycode. just for human speak!
+    [RequireOwner] ===> Requires the owner of the bot (YOU) to do the said command!
+    [RequireUserPermissions(GuildPermission.Your Wanted Permission)] ===> Requires the user doing the command to have a specified guild permission!!!
+    [RequireBotPermissions(GuildPermission.Your Wanted Permission)] ===> Requires the BOT to have a specified permission before operating the said command!
+    
+
+    Following a Pre-Condition , you must add a task! (Look at PublicModule.cs , Line 14 For example!)
+
+
+    All Said operations will require the await Operator! (Look at PublicModule.cs , Line 16 for example!)
+
+
+    That is all i have for you in this template! now its your turn to shine!
+
+    If you need help , @ me in the CWS Discord or at my personal discord: https://discord.gg/jYrQd5d 
+
+
+
+    ~Frost
+
+*/
---
+++ MyBot/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Discord.Net.Providers.WS4Net;
+using Discord.Net.Providers.UDPClient;
+
+namespace MyBot
+{
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            new Program().Start().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+
+        public async Task Start()
+        {
+
+            client = new DiscordSocketClient(new DiscordSocketConfig()
+            {
+                WebSocketProvider = WS4NetProvider.Instance,
+                UdpSocketProvider = UDPClientProvider.Instance,
+                LogLevel = LogSeverity.Verbose,
+            });
+
+            client.Log += Log;
+            await client.LoginAsync(TokenType.Bot, "MzE0MjkwMTU5MTY2MTYwOTE3.DATtdQ.hqFJUtx9M1m06IN0q92_a0gJr7o");
+            await client.StartAsync();
+
+            Console.WriteLine($"{DateTime.UtcNow}: Your Bot Has Started! Have fun kid ~Frost");
+
+            var serviceProvider = ConfigureServices();
+            handler = new CommandHandler(serviceProvider);
+            await handler.ConfigureAsync();
+
+
+            //Block this program untill it is closed
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+        public IServiceProvider ConfigureServices()
+        {
+
+            var services = new ServiceCollection()
+              .AddSingleton(client)
+             .AddSingleton(new CommandService(new CommandServiceConfig { CaseSensitiveCommands = false }));
+           // .AddSingleton<AudioService>();
+            var provider = new DefaultServiceProviderFactory().CreateServiceProvider(services);
+           // provider.GetService<AudioService>();
+       
+
+
+
+            return provider;
+        }
+
+
+    }
+}
