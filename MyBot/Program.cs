92f1c98aed3beeda7c440629b3e92e9ff18d7860
MisterMorde <morde@ninjafatcats.com>
2017-06-29 12:22:30 +1000

    le token fixed :p

--- MyBot/Program.cs
+++ MyBot/Program.cs
@@ -28,7 +28,7 @@ namespace MyBot
             });
 
             client.Log += Log;
-            await client.LoginAsync(TokenType.Bot, "MzE0MjkwMTU5MTY2MTYwOTE3.DATtdQ.hqFJUtx9M1m06IN0q92_a0gJr7o");
+            await client.LoginAsync(TokenType.Bot, "Insert Token Here "); // Previous Token has been closed :p
             await client.StartAsync();
 
             Console.WriteLine($"{DateTime.UtcNow}: Your Bot Has Started! Have fun kid ~Frost");
